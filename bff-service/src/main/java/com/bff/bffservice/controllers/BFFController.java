bdb0ddd3ce483094e872cb0dac9bd1408f3311ad
Eiad-Alaa <eiad.alaa.nouh@gmail.com>
2025-09-02 01:30:46 +0300

    Add logging producer to bff-service

--- bff-service/src/main/java/com/bff/bffservice/controllers/BFFController.java
+++ bff-service/src/main/java/com/bff/bffservice/controllers/BFFController.java
@@ -4,6 +4,7 @@ import com.bff.bffservice.dto.DashboardResp;
 import com.bff.bffservice.services.DashboardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,19 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.bff.bffservice.producer.LogProducer;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bff")
 public class BFFController {
 
+    @Autowired
     private final DashboardService dashboardService;
+
+    @Autowired
+    private LogProducer logProducer;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @GetMapping("/dashboard/{userId}")
     public Mono<ResponseEntity<DashboardResp>> getDashboard(@PathVariable UUID userId){
-
-        return dashboardService.getDashboard(userId)
+        logAsJson(userId, "Request");
+        Mono<ResponseEntity<DashboardResp>> resp = dashboardService.getDashboard(userId)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
+        logAsJson(resp, "Response");
+        return resp;
+    }
+
+    private void logAsJson(Object obj, String type) {
+        try {
+            String json = objectMapper.writeValueAsString(obj);
+            logProducer.sendLog(json, type);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
--- bff-service/src/main/java/com/bff/bffservice/exceptions/GlobalExceptionHandler.java
+++ bff-service/src/main/java/com/bff/bffservice/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.bff.bffservice.exceptions;
 
 import com.bff.bffservice.dto.ErrorResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Map;
-
+import com.bff.bffservice.producer.LogProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @Autowired
+    private LogProducer logProducer;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleUserNotFound(UserNotFoundException ex) {
@@ -19,7 +25,7 @@ public class GlobalExceptionHandler {
                 .error("User Not Found")
                 .message(ex.getMessage())
                 .build();
-
+        logAsJson(resp, "Response");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resp);
     }
 
@@ -30,8 +36,17 @@ public class GlobalExceptionHandler {
                 .error("Server Error")
                 .message("Error Occurred in Servers: " + ex.getMessage())
                 .build();
-
+        logAsJson(resp, "Response");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resp);
+    }
+
+    private void logAsJson(Object obj, String type) {
+        try {
+            String json = objectMapper.writeValueAsString(obj);
+            logProducer.sendLog(json, type);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
 }

9bad8a28784d6f0a650b69ec2fb5bd16fe4ca8d1
Eiad-Alaa <eiad.alaa.nouh@gmail.com>
2025-09-02 00:26:10 +0300

    Add logging producer to account-service

--- account-service/src/main/java/com/account/accountservice/controllers/AccountController.java
+++ account-service/src/main/java/com/account/accountservice/controllers/AccountController.java
@@ -11,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.account.accountservice.producer.LogProducer;
 
 import java.util.HashMap;
 import java.util.List;
@@ -24,43 +27,57 @@ public class AccountController {
     @Autowired
     private AccountService accService;
 
+    @Autowired
+    private LogProducer logProducer;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @PostMapping("/accounts")
     public ResponseEntity<CreateAccountRespDto> addAccount(@RequestBody @Valid CreateAccountReqDto request){
+        logAsJson(request, "Request");
         CreateAccountRespDto resp = accService.addAccount(request);
-
+        logAsJson(resp, "Response");
         return ResponseEntity.ok(resp);
     }
 
     @GetMapping("/accounts/{accountId}")
     public ResponseEntity<AccountDto>
     getAccount(@PathVariable @Valid UUID accountId){
-
+        logAsJson(accountId, "Request");
         AccountDto resp = accService.getAccountById(accountId);
-
+        logAsJson(resp, "Response");
         return ResponseEntity.ok(resp);
     }
 
     @GetMapping("/users/{userId}/accounts")
     public ResponseEntity<List<AccountDto>> getAccountsOfUser(@PathVariable UUID userId){
-
+        logAsJson(userId, "Request");
         List<AccountDto> resp = accService.getAccountsByUserId(userId);
-
+        logAsJson(resp, "Response");
         if(resp.isEmpty()){
             return ResponseEntity.noContent().build();
         }
-
         return ResponseEntity.ok(resp);
     }
 
     @PutMapping("/accounts/transfer")
     public ResponseEntity<Map<String , String>> transferAmount(@RequestBody @Valid TransferRequestDto req){
-
+        logAsJson(req, "Request");
         Map<String , String> resp = new HashMap<>();
 
         accService.transferAmount(req);
 
         resp.put("message" , "Accounts Updated Successfully");
-
+        logAsJson(resp, "Response");
         return ResponseEntity.ok(resp);
+    }
+
+    private void logAsJson(Object obj, String type) {
+        try {
+            String json = objectMapper.writeValueAsString(obj);
+            logProducer.sendLog(json, type);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
